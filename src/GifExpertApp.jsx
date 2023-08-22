9e60979e0f71adaa168e39380f49be2e31ffc571
Tomas <tomasmedero92@gmail.com>
2023-08-22 20:54:17 -0300

    name changed

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,47 +1,25 @@
 import { useState } from 'react'
-import { AddCategory, GifGrid } from './components';
-
-
+import { AddCategory, GifGrid } from './components'
 
 export const GifExpertApp = () => {
- 
-  const [categories, setCategories] = useState([ 'Dragon Ball' ]);
+  const [categories, setCategories] = useState(['Dragon Ball'])
 
   const onAddCategorie = (newCategory) => {
-
-
-      if (categories.includes(newCategory)) {
-
-        return;
-        
-      }
-      setCategories([newCategory ]);
-      
+    if (categories.includes(newCategory)) {
+      return
+    }
+    setCategories([newCategory])
   }
- 
+
   return (
     <>
+      <h1>Gif Expert App</h1>
 
-    <h1>GifExpertApp</h1>
+      <AddCategory newCategory={(value) => onAddCategorie(value)} />
 
-    <AddCategory 
-    
-    newCategory={(value) => onAddCategorie(value)}
-
-    />
-
-
-        { categories.map( (categoria) =>(
-        <GifGrid 
-        
-        key = {categoria} categoria={categoria}
-        
-        />
-        )
-          ) }
-      
-
-
+      {categories.map((categoria) => (
+        <GifGrid key={categoria} categoria={categoria} />
+      ))}
     </>
   )
 }
